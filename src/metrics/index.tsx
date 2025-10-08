7a17b41d7e8ba3e23dd0a806f33fdcfb3da4e3c4
Lương vũ anh tuấn <94426415+tuanlva20@users.noreply.github.com>
2025-10-08 14:39:20 +0700

    update GA4

--- src/metrics/index.tsx
+++ src/metrics/index.tsx
@@ -1,11 +1,9 @@
 // @project
 import GTag from './GTag';
-import MicrosoftClarity from './MicrosoftClarity';
-import Notify from './Notify';
 
 const clarityId = import.meta.env.VITE_APP_PUBLIC_CLARITY_ID || '';
 const notifyId = import.meta.env.VITE_APP_PUBLIC_NOTIFY_ID || '';
-const gaId = import.meta.env.VITE_APP_PUBLIC_ANALYTICS_ID || '';
+const gaId = import.meta.env.VITE_APP_PUBLIC_ANALYTICS_ID || 'G-ZJ1EC5JF9N';
 
 /***************************  METRICS  ***************************/
 
